9d19305894bff15469347b898701c77cb9a7b1ea
matan-workneh7 <matanworkneh77@gmail.com>
2025-09-14 19:53:53 +0300

    initisl commit with the log decorator containing the nececarry code in the wrapper for logging the sql

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,23 @@
+import sqlite3
+import functools
+
+# Decorator to log SQL queries
+def log_queries(func):
+    def wrapper(*args, **kwargs):
+        query = kwargs.get("query")
+        print(f"SQL query: {query}")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+# Fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
