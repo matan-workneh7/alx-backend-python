f36467bfc20a489101ae4b35ae2adecbd34aebae
matan-workneh7 <matanworkneh77@gmail.com>
2025-09-16 14:53:21 +0300

    fixed some errors

--- python-decorators-0x01/3-retry_on_failure.py
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -3,26 +3,28 @@ import sqlite3
 import functools
 
 #### paste your with_db_decorator here
-def with_db_connection(func):
+def with_db_connection(func):# this function accepts the 
     @functools.wraps(func)
     def Wrapper(*args, **kwargs):
-        conn = sqlite3.connect("user.db")
+        conn = sqlite3.connect("users.db")
         result = func(conn, *args ,*kwargs)
         return result
     return Wrapper
 
-def retry_on_failure(func):
-    def wrapper(*args, retries=3, delay=2, **kwargs):
-        last_exception = None
-        for attempt in range(retries):
-            try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                last_exception = e
-                print(f"Attempt {attempt + 1} failed: {e}. Retrying in {delay} seconds...")
-                time.sleep(delay)
-        raise last_exception
-    return wrapper  
+def retry_on_failure(retries=3, delay=1): #this accepts the function with the db connection parameter func right? but how will it take the retries and delays kwargs if the withdb function (func) only takes the conn
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            last_exception = None
+            for attempt in range(retries):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    last_exception = e
+                    print(f"Attempt {attempt + 1} failed: {e}. Retrying in {delay} seconds...")
+                    time.sleep(delay)
+            raise last_exception
+        return wrapper  
 
 @with_db_connection
 @retry_on_failure(retries=3, delay=1)
