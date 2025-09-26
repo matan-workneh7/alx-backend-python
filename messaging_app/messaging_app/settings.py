a8ab37fd3f02b8722e84853c14471418fd6ded51
matan-workneh7 <matanworkneh77@gmail.com>
2025-09-26 07:35:47 +0300

    added the REST_FRAMEWORK dictionary

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -124,3 +124,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    # By default, require authentication for all endpoints
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
