23d3257aa540a699e28463e1665fb69d824fb02d
matan-workneh7 <matanworkneh77@gmail.com>
2025-09-27 13:19:25 +0300

    created custom User model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,19 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Replace id with UUID
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # Extra fields from the spec
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    role = models.CharField(
+        max_length=10,
+        choices=[('guest', 'Guest'), ('host', 'Host'), ('admin', 'Admin')],
+        default='guest'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
