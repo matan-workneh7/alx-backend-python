efe617b107298a6d2d75447edea9d77dcc83168a
matan-workneh7 <matanworkneh77@gmail.com>
2025-09-29 21:29:12 +0300

    added the jwt timedelta and the jwt auth on the restframework dictionary

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,8 +134,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.JWTAuthentication', #Will ask for the JWT token
+        'rest_framework.authentication.SessionAuthentication', #This will be for fallback
     ],
 }
+
+# this setting will be specific to JWT
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60), #Makes the token last for 1hr
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1), #Makes the token refresh for 1day
+}
+
