49bd7ad5a1b835c952dbe83dd121e2bc95410190
matan-workneh7 <matanworkneh77@gmail.com>
2025-09-30 09:08:25 +0300

    fixes

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsOwner(BasePermission):
+class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
     
