5313dc70e5c98076ac6a6a9a0352e30f5a417544
matan-workneh7 <matanworkneh77@gmail.com>
2025-10-01 08:12:21 +0300

    made the first method to authenticate logged in users

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,4 +3,7 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
-    
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated #will be allowing only the logged-in users
