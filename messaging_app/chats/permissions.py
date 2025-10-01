f2fbc5b8cd2b3d75b6374162be281db50a711054
matan-workneh7 <matanworkneh77@gmail.com>
2025-10-01 20:43:35 +0300

    i added a method for checking if the users can access coversation and edit messages

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -7,3 +7,10 @@ class IsOwner(permissions.BasePermission):
 class IsParticipantOfConversation(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated #will be allowing only the logged-in users
+    
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, Conversation):
+            return request.user in obj.participants.all()
+        elif isinstance(obj, Message):
+            return request.user in obj.conversation.participants.all() 
+        return False
