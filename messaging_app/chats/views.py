364378d2df2973a0d2f0814b6231faf49d7a9f30
matan-workneh7 <matanworkneh77@gmail.com>
2025-10-04 21:00:10 +0300

    made the viewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation
+from .serializers import ConversationSerializer
+from .permissions import IsParticipantOfConversation
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsParticipantOfConversation]
