4005d3d4dee1d0e18d17707db5f479ea3e60a31c
matan-workneh7 <matanworkneh77@gmail.com>
2025-10-04 21:26:15 +0300

    added the dafe_methods for user that has access to edit and others just to view and some spacings

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,16 +1,24 @@
 from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
+    
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
 
 class IsParticipantOfConversation(permissions.BasePermission):
+
     def has_permission(self, request, view):
         return request.user.is_authenticated #will be allowing only the logged-in users
     
     def has_object_permission(self, request, view, obj):
+
         if isinstance(obj, Conversation):
             return request.user in obj.participants.all()
+        
         elif isinstance(obj, Message):
-            return request.user in obj.conversation.participants.all() 
+            if request.method in permissions.SAFE_METHODS:
+                return request.user in obj.conversation.participants.all()
+            
+            elif request.method in ['PUT', 'PATCH', 'DELETE']:
+                return request.user == obj.user
         return False
