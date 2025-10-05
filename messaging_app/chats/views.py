cebad92081b15b756eb16c7ce94fa1f093b6973c
matan-workneh7 <matanworkneh77@gmail.com>
2025-10-05 12:41:45 +0300

    created the message custome get_query method added the necessary imports and made the create message method with the necessary athentication and response

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,9 +1,39 @@
-from rest_framework import viewsets
-from .models import Conversation
-from .serializers import ConversationSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipantOfConversation
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsParticipantOfConversation]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+    def get_queryset(self):
+        conversation_id = self.kwargs.get("conversation_id")
+        conversation = Conversation.objects.filter(id=conversation_id).first()
+
+        # If there was no conversation found or user not a participant → forbid
+        if not conversation or self.request.user not in conversation.participants.all():
+            return Message.objects.none()  # empty queryset
+
+        return Message.objects.filter(conversation=conversation)
+
+    def create(self, request, *args, **kwargs):
+        conversation_id = self.kwargs.get("conversation_id")
+        conversation = Conversation.objects.filter(id=conversation_id).first()
+
+        if not conversation or request.user not in conversation.participants.all():
+            return Response(
+                {"detail": "You are not allowed to send messages in this conversation."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(sender=request.user, conversation=conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
