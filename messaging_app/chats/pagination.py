8f10afebe876ba6c6ea6ed45108a45fcd82afa9e
matan-workneh7 <matanworkneh77@gmail.com>
2025-10-05 20:46:36 +0300

    added the page counter

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,16 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        
+        return Response({
+            "count": self.page.paginator.count,      # total messages
+            "next": self.get_next_link(),            # link to next page
+            "previous": self.get_previous_link(),    # link to previous page
+            "results": data                          # actual message data
+        })
